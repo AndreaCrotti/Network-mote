80c713ffe115cac5354ff963b58621c0286b74c2
Oscar Dustmann <oscar.dustmann@rwth-aachen.de>
2010-06-22 16:50:45 +0200

    non compiling project

---
+++ driver/main.c
@@ -0,0 +1,23 @@
+//include <sys/socket.h>
+#include <linux/if.h>
+#include "tun_dev.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int args, char** arg) {
+  char dev[IFNAMSIZ];
+  memset(dev,0,IFNAMSIZ);
+  *dev = 42;
+  int fd = tun_open(dev);
+  if (fd < 1) {
+    printf("hat nich geklappt! :(\n");
+  } else {
+    char cpy[IFNAMSIZ+2];
+    memset(cpy,0,IFNAMSIZ+2);
+    strncpy(cpy,dev,IFNAMSIZ);
+    printf("whooohooo: %s\n",cpy);
+  }
+  return 0;
+}
