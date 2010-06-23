145b3a346be728d73fb93920373bfee115db82c1
Marius Grysla <marius@marius-laptop.(none)>
2010-06-23 16:16:08 +0200

    First simple but COMPILING version that creates a new tun device!

--- driver/main.c
+++ driver/main.c
@@ -1,23 +1,29 @@
-//include <sys/socket.h>
-#include <linux/if.h>
-#include "tun_dev.h"
 
+// Includes
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+
+// Standart libraries
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
+// Tunnel device
+#include "tun_dev.h"
+
 int main(int args, char** arg) {
     char dev[IFNAMSIZ];
-    memset(dev, 0, IFNAMSIZ);
-    *dev = 42;
+    char input[20];
+    dev[0] = 0;
+
     int fd = tun_open(dev);
-    if (fd < 1) {
-        printf("hat nich geklappt! :(\n");
-    } else {
-        char cpy[IFNAMSIZ + 2];
-        memset(cpy, 0, IFNAMSIZ+2);
-        strncpy(cpy, dev, IFNAMSIZ);
-        printf("whooohooo: %s\n", cpy);
-    }
+    
+    fgets(input, 20, stdin);
+
+    tun_close(fd, dev);
+
     return 0;
 }
