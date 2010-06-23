edfde971ad92afc393addf7e0421f2d3f621dbff
Marius Grysla <marius@marius-laptop.(none)>
2010-06-23 16:43:53 +0200

    Loop

--- driver/main.c
+++ driver/main.c
@@ -1,23 +1,41 @@
-//include <sys/socket.h>
-#include <linux/if.h>
-#include "tun_dev.h"
 
+// Includes
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+
+// Standart libraries
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
+// Tunnel device
+#include "tun_dev.h"
+
 int main(int args, char** arg) {
     char dev[IFNAMSIZ];
-    memset(dev, 0, IFNAMSIZ);
-    *dev = 42;
+    char input[20];
+
+    // a new device should be opened!
+    dev[0] = 0;
+    
     int fd = tun_open(dev);
-    if (fd < 1) {
-        printf("hat nich geklappt! :(\n");
-    } else {
-        char cpy[IFNAMSIZ + 2];
-        memset(cpy, 0, IFNAMSIZ+2);
-        strncpy(cpy, dev, IFNAMSIZ);
-        printf("whooohooo: %s\n", cpy);
+    
+    //fgets(input, 20, stdin);
+
+    uint8_t buf[sizeof(struct split_ip_msg) + INET_MTU];
+    struct split_ip_msg *msg = (struct split_ip_msg *)buf;
+    int len;
+
+    while(1){
+        len = tun_read(fd, (void *)(&msg->pi), INET_MTU + sizeof(struct ip6_hdr));
+        if(len != -5)
+            printf("%d", len);
     }
+
+    tun_close(fd, dev);
+
     return 0;
 }
