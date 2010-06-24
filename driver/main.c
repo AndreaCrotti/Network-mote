5cb0aa47b19e6ba554c5d5e657a828c0fef288c2
Marius Grysla <marius@marius-laptop.(none)>
2010-06-24 14:42:14 +0200

    Some additional output

--- driver/main.c
+++ driver/main.c
@@ -11,19 +11,22 @@
 #include <string.h>
 #include <stdio.h>
 
-// Tunnel device
+// Functions for using a tunnel device
 #include "tun_dev.h"
+// For adding an entry to the routing tables
+#include "routing.h"
 
 int main(int args, char** arg) {
     char dev[IFNAMSIZ];
     char input[20];
 
     // a new device should be opened!
-    dev[0] = 0;
-    
+    dev[0] = 0;    
     int fd = tun_open(dev);
     
-    //fgets(input, 20, stdin);
+    // Testing...
+    printf("IPV6_VERSION is: %d", IPV6_VERSION);
+    fflush(stdout);
 
     uint8_t buf[sizeof(struct split_ip_msg) + INET_MTU];
     struct split_ip_msg *msg = (struct split_ip_msg *)buf;
@@ -31,8 +34,24 @@ int main(int args, char** arg) {
 
     while(1){
         len = tun_read(fd, (void *)(&msg->pi), INET_MTU + sizeof(struct ip6_hdr));
-        if(len != -5)
-            printf("%d", len);
+        //if(len != -5)
+            //printf("%d", len);
+
+        printf
+
+        if (len > 0) {
+            printf("tun_read: read 0x%x bytes\n", len);
+
+            if ((msg->hdr.vlfc[0] >> 4) != IPV6_VERSION) {
+                printf("tun_read: discarding non-ip packet\n");
+            } else if (ntohs(msg->hdr.plen) > INET_MTU - sizeof(struct ip6_hdr)) {
+                printf("tun_input: dropping packet due to length: 0x%x\n", ntohs(msg->hdr.plen));
+            } else if (msg->hdr.nxt_hdr == 0) {
+                printf("tun_input: dropping packet with IPv6 options\n");
+            }
+        }
+        fflush(stdout);
+        usleep(100);
     }
 
     tun_close(fd, dev);
