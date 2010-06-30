254cb98ae17c958e1d7250715cc7545b0bb3dac5
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-06-30 13:19:56 +0200

    split_packet only needs the maximum size now

--- driver/test_packets.c
+++ driver/test_packets.c
@@ -1,15 +1,19 @@
 // -*- compile-command: "gcc -Wall -Wextra -lm -lpcap -o test test_packets.c && ./test" -*-
+// TODO: add a checksum field
+// TODO: try with real IPv4 packets and try to send them over the localhost interface
+
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
 #include <pcap.h>
+/* #include <netinet/ip6.h> */
 
 #define LEN 100
-#define N_CHUNKS 400
+#define N_BYTES 8
 
-void **split_binary(const void *, int, int);
+void **split_binary(const void *, int);
 void *rebuild(void **, int, int);
 
 int main() {
@@ -20,26 +24,29 @@ int main() {
         data[i] = i;
     }
     int nbytes = LEN * sizeof(int);
-    void **chunks = split_binary(data, nbytes, N_CHUNKS);
+    void **chunks = split_binary(data, N_BYTES);
     // now check if they're equivalent
-    int *reconstructed = (int *) rebuild(chunks, nbytes, N_CHUNKS);
+    int *reconstructed = (int *) rebuild(chunks, nbytes, N_BYTES);
     // use assert to check for equivalence
     for (i = 0; i < LEN; i++) {
         /* printf("data[i] = %d, reconstructed[i] = %d\n", data[i], reconstructed[i]); */
         assert(data[i] == (int) reconstructed[i]);
     }
+    // now we should free them also
+    free(reconstructed);
+    free(chunks);
     return(0);
 }
 
 
 // splitting the data in binary chunks of len len, returning
 // a pointer to the array of chunks
-void **split_binary(const void *data, int size, int num_chunks) {
+void **split_binary(const void *data, int chunk_size) {
     // allocate enough pointers here, make sure "int" is doing the ceil!
-    int chunk_size = (ceil) ((float) size / num_chunks);
     int i;
+    int num_chunks = (ceil) ((float) sizeof(data) * LEN / chunk_size);
     void **result = malloc(sizeof(void *) * num_chunks);
-    printf("allocating %d times %d bytes\n", num_chunks, chunk_size);
+    printf("allocating %d times %d bytes and tot = %ld\n", num_chunks, chunk_size, sizeof(data)*LEN);
 
     for (i = 0; i < num_chunks; i++) {
         result[i] = calloc(1, chunk_size);
@@ -48,9 +55,9 @@ void **split_binary(const void *data, int size, int num_chunks) {
     return result;
 }
 
-void *rebuild(void **chunks, int size, int num_chunks) {
+void *rebuild(void **chunks, int size, int chunk_size) {
     void *data = malloc(size);
-    int chunk_size = (ceil) ((float) size / num_chunks);
+    int num_chunks = (ceil) ((float) size / chunk_size);
     int i;
     for (i = 0; i < num_chunks; i++) {
         memcpy(data + (i * chunk_size), chunks[i], chunk_size) ;
