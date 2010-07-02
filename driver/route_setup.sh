1b1ab1746f43417e47eebbe385b80dcdd9dbd53f
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-07-02 16:58:04 +0200

    more code in setup the route

--- driver/route_setup.sh
+++ driver/route_setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # setup the TUN device tun$1 as default gateway for ipv6
+# first make sure that the device exists, call tun_setup to set it up
+TUN="tun$1"
+ADDR="fec0::1"
+
 if [ ! $(which ip) ]
 then
     echo "you need iproute2 to setup ipv6 routing"
     exit 1
 fi
 
+ifconfig $TUN $ADDR
 # check if the address is correct
-ip route add default via fec0::1 dev tun$1
+ip route add default via $ADDR dev tun$1
