af125c9f4e6ccf49cbfe39ff04bdd185ff4c6a69
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-07-13 16:08:38 +0200

    bridge

--- driver/launch.sh
+++ driver/launch.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# we can pass some options here an
-# operations to do in case we want to create a bridge could be
-
-# brctl addbr br0
-# brctl addif br0 eth0
-# ifconfig eth0 down
-# ifconfig br0 192.168.1.1
-
 # an then all the traffic will go throgh the bridge automatically
 # there could be some options
 
