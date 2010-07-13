97a9d5602e7fcb72b38750eb40d70f218a1e689f
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-07-13 10:14:49 +0200

    trying the select with two tun devices

--- python/main.py
+++ python/main.py
@@ -8,6 +8,8 @@ from scapy.all import *
 # from TOSSIM import Tossim, SerialForwarder, Throttle
 import os
 import zlib
+from select import select
+import socket
 
 TOSROOT = os.getenv("TOSROOT")
 if TOSROOT is None:
@@ -25,7 +27,7 @@ from tinyos.tossim.TossimApp import NescApp
 # s.sendto(MAGIC_WORD, peer)
 
 # setup the correct route via the created device
-def setup_device(num, mode='tun'):
+def setup_device(ipaddr, mode='tun'):
     # creating a tun device and sending data on it
     TUNSETIFF = 0x400454ca
     # those values should be read in the if_tun.h directly somehow
@@ -37,34 +39,48 @@ def setup_device(num, mode='tun'):
     else:
         TUNMODE = IFF_TUN
 
-    f = os.open("/dev/net/tun", os.O_RDWR)
-    ifs = ioctl(f, TUNSETIFF, struct.pack("16sH", "toto%d", TUNMODE))
+    fd = os.open("/dev/net/tun", os.O_RDWR)
+    ifs = ioctl(fd, TUNSETIFF, struct.pack("16sH", "toto%d", TUNMODE))
     ifname = ifs[:16].strip("\x00")
     print "Allocated interface %s. Configure it and use it" % ifname
-    ipaddr = "10.0.0.%s" % str(num)
     os.popen("ifconfig %s %s netmask 255.255.255.0" % (ifname, ipaddr))
-    return ipaddr
+    return fd
 
-def compress():
-    MAXSIZE = 100
+def compress(packet, dst, size=100):
     # conf.route6.add
     # maybe we can also sniff instead of reading on the device
-    while True:
-        p = sniff(count=1, filter="ip6")[0]
-        compressed = zlib.compress(str(p), 9)
-        pkts = [compressed[x:x+MAXSIZE] for x in range(len(compressed))]
-        # we gain about the 30% over the network
-        print "from %s generated %s packets of size %d, gaining %d" % (compressed, len(pkts), MAXSIZE, len(str(p)) - len(compressed))
-        # now we can send them and reconstruct them somewhere else
-
-devs = [setup_device(1), setup_device(2)]
+    compressed = zlib.compress(str(packet), 9)
+    # should also setup the source and other fields
+    # get the slicing
+    splits = [compressed[x:x+size] for x in range(0, len(compressed), size)]
+    return [IPv6(dst=dst) / x for x in splits]
 
 payload = "very long message" * 100
-import multiprocessing
 
 # proof of concept, create 2 virtual device, and start some multiprocessing magic with them
-def proof(num):
-    if num == 2:
-        dev = setup_device(2)
-    # also check that the ordering is correct
-    
+def proof():
+    ipaddr = "10.0.0.%d"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    devs = setup_device(ipaddr % 1), setup_device(ipaddr % 2)
+    try:
+        while True:
+            ro, wr, _ = select(devs, devs, [])
+            print ro, wr
+            # first try to read on one of them, otherwise write
+            if ro != []:
+                for x in ro:
+                    pkt = os.read(x, 1024)
+                    print "%d has received packet %s" % (x, str(pkt))
+            else:
+                # otherwise we write something on both
+                for x in wr:
+                    print "writing the payload to %d" % x
+                    os.write(x, payload)
+                    # sending with a socket to the other address
+                    other = devs[(devs.index(x) + 1) % 2]
+
+    except KeyboardInterrupt:
+        for d in devs:
+            print "closing %d" % d
+            os.close(d)
+proof()
