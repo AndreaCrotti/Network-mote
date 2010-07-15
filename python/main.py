fbcc3a9d5ed4a45e4c05a845ee78977d1a35b242
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-07-15 19:02:11 +0200

    remove old stuff

--- python/main.py
+++ python/main.py
@@ -21,6 +21,7 @@ from scapy.all import IPv6
 
 DEFCOMPRESS = True
 MYHEADER = 'Hhl'
+POPEN = lambda cmd: subprocess.Popen(cmd, shell=True)
 
 TOSROOT = os.getenv("TOSROOT")
 if TOSROOT is None:
@@ -170,93 +171,9 @@ class Merger(object):
         self.compression = compression
 
     # see how we can decompress not knowing the dimension
+    # we can decompress on the fly or create the big chunk and decompress in the end
     def merge(self):
         pass
-
-def reconstruct(packets):
-    "Reconstruct the original data from the compressed packets"
-    print "len = %d" % len(packets[0].payload)
-    restored = [struct.unpack("!hHL%ds" % max_size,  p.payload) for p in packets]
-    # grouping by sequential number and sorting by ord number after
-    data = ""
-    for v in sorted(restored, key=lambda x: x[1]):
-        print v
-        data += v[3]
-    # now we can finally uncompress the data
-    # orig = zlib.decompress(data)
-    return data
-
-def test_compress():
-    p = sniff(count=10)
-    # print compress(str(p), "::1")
-
-# proof of concept, create 2 virtual device, and start some multiprocessing magic with them
-def proof():
-    ipaddr = "10.0.0.%d"
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    devs = setup_device(ipaddr % 1), setup_device(ipaddr % 2)
-    try:
-        while True:
-            ro, wr, _ = select(devs, devs, [])
-            print ro, wr
-            # first try to read on one of them, otherwise write
-            if ro != []:
-                for x in ro:
-                    pkt = os.read(x, 1024)
-                    print "%d has received packet %s" % (x, str(pkt))
-            else:
-                # otherwise we write something on both
-                for x in wr:
-                    print "writing the payload to %d" % xb
-                    os.write(x, payload)
-                    # sending with a socket to the other address
-                    other = devs[(devs.index(x) + 1) % 2]
-
-    except KeyboardInterrupt:
-        for d in devs:
-            print "closing %d" % d
-            os.close(d)
-
-def test_select():
-    # trying out a select using a couple of devices
-    sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # 1 is the client and we do the selection on 2 and 3
-    addr1, addr2 = (("", 10000), ("", 100000))
-    sock2.bind(addr1)
-    sock3.bind(addr2)
-
-    # this select stuff with sockets works pretty well
-    from random import random
-    while True:
-        if random() > 0.5:
-            sock1.sendto("ciao ", addr1)
-        else:
-            sock1.sendto("ciao ", addr2)
-        r, w, _ = select([sock2, sock3], [], [])
-        print r,w
-        for x in r:
-            print "reading data %s" % str(x.recv(1024))
-
-def test_usb_writing():
-    dev = "/dev/ttyUSB0"
-    fd = os.open(dev, os.O_RDWR)
-    # try to read and write from that and see what happens
-
-
-# rewrite this using only scapy
-def test_mtu_speed():
-    t = TunTap('tap', 1024)
-    t.setup()
-    addr = "10.0.1.1"
-    subprocess.Popen("/sbin/ifconfig tap0 %s" % addr, shell=True)
-    for mtu in range(50, 1000, 100):
-        print "\n\nfor mtu %d we have ping" % mtu
-        subprocess.Popen("ifconfig tap0 mtu %d" % mtu, shell=True)
-        os.popen("/sbin/ifconfig tap0 mtu %d" % mtu).read()
-        # then ping and see how fast it is
-        subprocess.Popen("ping -p ff -c 5 %s" % addr, shell=True)
 
 def usage():
     print "usage: ./main.py <device>"
