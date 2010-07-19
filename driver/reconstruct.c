d6bea537fd11aa167b202d2376299d9e256de4f5
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-07-19 14:13:07 +0200

    correcting the reconstruct. Now it works as a circular list, it will write in order 1 2 3 4 2 3 4 5 3 4 5 6
    and so on...

--- driver/reconstruct.c
+++ driver/reconstruct.c
@@ -13,12 +13,16 @@
 // Maybe we could use this http://www.cl.cam.ac.uk/~cwc22/hashtable/ as data structure
 
 // FIXME: now when the index changes we don't find it anymore
-#define MAX_SEQ (index + MAX_RECONSTRUCTABLE)
-#define POS(x) (x - index)
+#define NEXT_POS(x) ((x + 1) % MAX_RECONSTRUCTABLE)
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
 
 myPacketHeader *recast(stream_t *data);
 void move_forward(int seq_no);
-void resetChunks(int seq_no);
+void resetPacket(packet_t *actual, myPacketHeader *original);
+void testRecast(myPacketHeader *p);
 
 // just using a send function would be fine
 static void (*globalcallback)(myPacketHeader *completed);
@@ -30,6 +34,9 @@ static int index = 0;
 // pass a callback function to send somewhere else the messages when they're over
 void initReconstruction(void (*callback)(myPacketHeader *completed)) {
     int i, j;
+    if (DEBUG)
+        printf("initializing the reconstruction\n");
+
     globalcallback = callback;
     for (i = 0; i < MAX_RECONSTRUCTABLE; i++) {
         // is it always a new packet_t right?
@@ -44,57 +51,47 @@ void initReconstruction(void (*callback)(myPacketHeader *completed)) {
 }
 
 /** 
- * Accessing directly to the correct spot in the array
+ * Add a new chunk to the list of temp
  * 
- * @param seq 
- * 
- * @return 
+ * @param data 
  */
-int getPosition(int seq) {
-    // get the seq in last position and go backward
-    return (MAX_RECONSTRUCTABLE - (MAX_SEQ - seq));
-}
-
 void addChunk(void *data) {
-    myPacketHeader *p = recast(data);
-    int seq_no = p->seq_no;
-    int ord_no = p->ord_no;
-    // does nothing if we're in the limit
-    move_forward(seq_no);
+    myPacketHeader *original = recast(data);
+    int seq_no = original->seq_no;
+    int ord_no = original->ord_no;
     
     // just for readability
-    packet_t *actual = &temp_packets[POS(seq_no)];
-    // check if first time we receive a chunk of this packet
-    if (actual->seq_no >= 0) {
-        actual->missing_chunks = p->parts;
-        actual->seq_no = seq_no;
-    } else {
-        resetChunks(seq_no);
+    packet_t *actual = &temp_packets[index];
+
+    // we have to overwrite everything in case we're overwriting OR
+    // is the first chunk with that seq_no that we receive
+    if (actual->seq_no != seq_no) {
+        if (DEBUG)
+            printf("overwriting or creating new packet at position %d\n", index);
+
+        resetPacket(actual, original);
     }
-    
+    if (DEBUG) 
+        printf("adding chunk ord_no = %d to seq_no = %d\n", ord_no, seq_no);
+
+    actual->seq_no = seq_no;
     // check not receiving same data twice
     assert(actual->chunks[ord_no] == 0);
     // now add the chunk (using the payload
     /* actual.chunks[ord_no] = (stream_t *); */
     actual->missing_chunks--;
+
+    index = NEXT_POS(index);
 }
 
 // reset all the chunks at that sequential number
-void resetChunks(int seq_no) {
-    packet_t *actual = &temp_packets[getPosition(seq_no)];
+void resetPacket(packet_t *actual, myPacketHeader *original) {
     int i;
-
-    actual->seq_no = seq_no;
+    actual->seq_no = original->seq_no;
+    actual->missing_chunks = original->parts;
+        
     for (i = 0; i < MAX_CHUNKS; i++) {
         actual->chunks[i] = 0;
-    }
-}
-
-void move_forward(int seq_no) {
-    int offset = (seq_no - MAX_SEQ);
-    if (offset > 0) {
-        index += offset;
-        printf("we'll overwrite everything below %d\n", index);
     }
 }
 
@@ -122,6 +119,7 @@ int main(int argc, char *argv[]) {
     // assertions to check we really have those values there
     
     free(pkt);
+    return 0;
 }
 
 void testRecast(myPacketHeader *p) {
