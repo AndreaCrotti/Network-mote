481ffbdd2090c715a51d4cad25691ff811204c4d
Oscar Dustmann <oscar.dustmann@rwth-aachen.de>
2010-07-23 14:36:10 +0200

    Makefile tests

---
+++ driver/motecomm.test.c
@@ -0,0 +1,40 @@
+#include "motecomm.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** args) {
+  //
+  mcp_header_t mht;
+  mht.stream = (stream_t*)malloc(100);
+  mht.header->version = 1;
+  mht.header->header = 5;
+  mht.header->ident = 0xDE;
+  mht.header->type = 0xAD;
+  mht.header->port = 0xBE;
+  mht.header->payload = 0xEF;
+  int i = 0;
+  printf("If it works, you should see '15 DE AD BE EF'\n");
+  printf("NX_SWAP_NIBBLES: %u\n",(unsigned)NX_SWAP_NIBBLES);
+  for (; i < MCP_HEADER_BYTES; i++) {
+    printf("%02X ",(unsigned)(mht.stream[i]));
+  }
+  return 0;
+  assert(argc >= 3);
+  mcp_t* mcp = openMcpConnection(args[1],args[2],NULL);
+  if (!mcp) {
+    printf("There was an error opening the connection to %s over device %s.",args[2],args[1]);
+    return 1; 
+  }
+  assert(mcp);
+  assert(mcp->getComm);
+  assert(mcp->getComm(mcp));
+  assert(mcp->getComm(mcp)->read);
+  mccmp(NULL,mcp);
+  laep(NULL,mcp);
+  ifp(NULL,mcp);
+  while (1) {
+    mcp->getComm(mcp)->read(mcp->getComm(mcp));
+    printf("there was a message\n");
+  }
+  return 0;
+}
