855e13bdd079259d5e96058e766400c2f055d41f
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-07-25 22:26:19 +0200

    uncommenting in header also

--- driver/tunnel.h
+++ driver/tunnel.h
@@ -43,15 +43,15 @@ int getFd(int client_no);
  */
 int tunRead(int client_no, char *buf, int n);
 
-/* /\**  */
-/*  * Add one message to the write queue */
-/*  * and then try to send them all out */
-/*  *  */
-/*  * @param client_no client connected */
-/*  * @param buf buffer to send */
-/*  * @param len length of the buffer */
-/*  *\/ */
-/* void addToWriteQueue(int client_no, payload_t data); */
+/**
+ * Add one message to the write queue
+ * and then try to send them all out
+ *
+ * @param client_no client connected
+ * @param buf buffer to send
+ * @param len length of the buffer
+ */
+void addToWriteQueue(int client_no, payload_t data);
 
 /** 
  * Write on tun device without using the fancy queue
