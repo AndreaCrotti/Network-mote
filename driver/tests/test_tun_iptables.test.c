98a3a918801506e9837b9f4df6e989e94ce50e25
Marius Grysla <marius@marius-laptop.(none)>
2010-07-26 15:32:40 +0200

    A more suitable test for the gateway

--- driver/tests/test_tun_iptables.test.c
+++ driver/tests/test_tun_iptables.test.c
@@ -8,10 +8,12 @@
 #include <fcntl.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
+#include "../setup.h"
 
 char msg[] = "hallo hallo\n";
 #define TUN_DEV "/dev/net/tun"
 char ipaddr[] = "10.0.0.1";
+char setup_script[] = "./test_tun_iptables.sh";
 
 int create_tun(char *dev) {
     struct ifreq ifr;
@@ -19,7 +21,7 @@ int create_tun(char *dev) {
     char *clonedev = TUN_DEV;
 
     // open the tun device rw mode
-    int fd = open(clonedev , O_RDONLY);
+    int fd = open(clonedev , O_RDWR);
     if (fd < 0) {
         perror("Opening /dev/net/tun");
         exit(1);
@@ -45,6 +47,7 @@ int create_tun(char *dev) {
     // Write the name of the new interface to device
     strcpy(dev, ifr.ifr_name);
     printf("successfully created %s\n", dev);
+
     return fd;
 }
 
@@ -57,22 +60,36 @@ int read_from(int fd, char *buf, int len) {
 }
 
 /* int write_with_socket(int sock) { */
-/*     int sd = socket(PF_INET, SOCK_RAW, IPPROTO_UDP); */
+/*     int sd = socket(PF_INET,sdfsdfw SOCK_RAW, IPPROTO_UDP); */
     
 /*     return 0; */
-/* } */
+/* } */ 
 
 int main() {
     char name[IFNAMSIZ];
     name[0] = 0;    
     int fd = create_tun(name);
-
-    /* int wrote = write_tun_write(fd); */
-    /* printf("wrote %d bytes on the tun device, now check iptables log\n", wrote); */
     
+    // Setting up IP-tables routes
+    char script_call[strlen(setup_script) + 1 + IFNAMSIZ];
+    strncpy(script_call, setup_script, strlen(setup_script) + 1);
+    strncpy(script_call + strlen(setup_script), " ", 1);
+    strncpy(script_call + strlen(setup_script) + 1, name, IFNAMSIZ);
+    printf("Calling script with %s\n", script_call); 
+    fflush(stdout);
+    callScript(script_call, "Created iptable entries\n", 
+               "An error occured during creaton of iptable entries", 0);
+
+    for(;;){
+        int wrote = write_tun_write(fd);
+        printf("wrote %d bytes on the tun device, now check iptables log\n", wrote);
+        usleep(500000);
+    }
+
     char buf[strlen(msg) + 1];
+    
     // read is blocking here 
-    read_from(fd, buf, strlen(msg));
+    // read_from(fd, buf, strlen(msg));
     printf("reading from the device string\n");
     return 0;
 }
