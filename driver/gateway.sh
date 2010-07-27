30a5a04696a7a7c0a698ded38a076925e27313da
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-07-27 12:02:05 +0200

    gateway rules

--- driver/gateway.sh
+++ driver/gateway.sh
@@ -14,23 +14,25 @@ then
 fi
 
 TUN=$1
-TUN_IP=10.0.0.254
+GATEWAY_IP=10.0.0.254
+CLIENT_IP=10.0.0.1
 ETH=$2
 ETH_IP=$(ifconfig $ETH | grep "inet addr:" | awk -F: '{ print $2 }' | awk '{ print $1 }')
 
 ip link set $TUN up
-ip addr add $TUN_IP dev $TUN
+ip addr add $GATEWAY_IP dev $TUN
 
 $IPT -t nat -F
 $IPT -Z
 
 # from what they say on the internet THIS rule should be enough
-$IPT -A POSTROUTING -t nat -s $TUN_IP -o $ETH -j SNAT --to-source $ETH_IP --log-prefix "Packet coming in on tun: " --log-level 7
+# $IPT -A POSTROUTING -t nat -s $TUN_IP -o $ETH -j SNAT --to-source $ETH_IP
 
 # or also this one
-# $IPT -t nat -I POSTROUTING -j MASQUERADE
-
-# $IPT -t nat -A POSTROUTING -o $ETH -j LOG --log-prefix "Packet coming in on tun: " --log-level 7
+# insert a nat rule in the postrougint which jumps to masquerading
+$IPT -t nat -I POSTROUTING -j MASQUERADE
+# -o $TUN 
+$IPT -t mangle -A FORWARD --destination $CLIENT_IP -j LOG --log-prefix "packets on eth: " --log-level 7
 # #$IPT -A OUTPUT -o $TUN -j LOG --log-prefix "Packet going out to tun" --log-level 7
 
 # $IPT -t nat -A PREROUTING -i $TUN -j DNAT --to-destination $ETH_IP
