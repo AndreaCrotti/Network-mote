b8e31f52b81f1851792369ef8d74caff2dceb7d5
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-08-04 01:19:24 +0200

    actually exit when closing everything

--- driver/setup.c
+++ driver/setup.c
@@ -23,6 +23,7 @@ void _close_everything(int param) {
     // closing tunnel and compression module
     close_all_tunnels();
     close_compression();
+    exit(EXIT_SUCCESS);
 }
 
 void main_loop(fdglue_t *fdg) {
