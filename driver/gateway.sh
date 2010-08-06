67cfba4688e8ff14148edb34f1d98e629e3f5e53
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-08-06 11:29:18 +0200

    -q in sysctl

--- driver/gateway.sh
+++ driver/gateway.sh
@@ -3,7 +3,7 @@
 # see http://www.revsys.com/writings/quicktips/nat.html to understand it
 
 #set -x
-sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.ip_forward=1 -q
 
 IPT="iptables"
 
@@ -26,20 +26,8 @@ ip route add 10.0.0.0/24 via $GATEWAY_IP dev $TUN
 $IPT -t nat -F
 $IPT -Z
 
-# from what they say on the internet THIS rule should be enough
-# $IPT -A POSTROUTING -t nat -s $TUN_IP -o $ETH -j SNAT --to-source $ETH_IP
-
 # or also this one
 # insert a nat rule in the postrougint which jumps to masquerading
 $IPT -t nat -I POSTROUTING -j MASQUERADE
 # -o $TUN 
 $IPT -t mangle -A FORWARD --destination $CLIENT_IP -j LOG --log-prefix "packets on eth: " --log-level 7
-# #$IPT -A OUTPUT -o $TUN -j LOG --log-prefix "Packet going out to tun" --log-level 7
-
-# $IPT -t nat -A PREROUTING -i $TUN -j DNAT --to-destination $ETH_IP
-
-# $IPT -t nat -A POSTROUTING -o $ETH -j MASQUERADE
-# # accept stuff from the network which is connected
-# $IPT -A FORWARD -i $ETH -o $TUN -m state --state RELATED,ESTABLISHED -j ACCEPT
-# # accept everything from tap to external network
-# $IPT -A FORWARD -i $TUN -o $ETH -j ACCEPT
